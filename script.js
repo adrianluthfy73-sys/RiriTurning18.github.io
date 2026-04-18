d53e35ddc92a306fceda8dafc9d9652117e50866
adrianluthfy73-sys <adrianluthfy73@gmail.com>
2026-04-18 15:33:14 +0700

    Update ringTexts array in script.js with new personalized messages for Riri's 18th birthday.

---
+++ script.js
@@ -0,0 +1,6 @@
+const ringTexts = [
+    "Happy Birthday Riri",
+    "Makasih ya Ri, udah jadi kamu versi yang paling aku hebat bukan karena sempurna tapi karena kamu jadi dirimu sendiri",
+    "Ke depannya masih panjang, tapi kamu nggak perlu khawatir. Aku akan selalu menemanimu ditiap langkahnya",
+    "Happy birthday 18th Riri Love U So Much"
+];
